95573fe7601bfed573dfc59d5acc513e162d07bc
Oltion-Koceku <koceku17@gmail.com>
2024-05-09 14:31:21 +0200

    [first]

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,23 @@
+<footer class="d-flex">
+    <div class="container d-flex">
+      <div>
+        <h4>Boolando s.r.l</h4>
+        <nav>
+          <ul class="d-flex column">
+            <li><a href="#">Infomazioni legali</a></li>
+            <li><a href="#">Normativa sulla privacy</a></li>
+            <li><a href="#">Diritto di recesso</a></li>
+          </ul>
+        </nav>
+      </div>
+
+      <div class="social">
+        <h4>Trovaci anche su</h4>
+        <a href=""><i class="fa-brands fa-x-twitter"></i></a>
+        <a href=""><i class="fa-brands fa-square-facebook"></i></a>
+        <a href=""><i class="fa-brands fa-instagram"></i></a>
+        <a href=""><i class="fa-brands fa-pinterest"></i></a>
+        <a href=""><i class="fa-brands fa-youtube"></i></a>
+      </div>
+    </div>
+  </footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,38 @@
+<header>
+    <div class="topbar">
+      <div class="container d-flex box">
+        <!-- nav -->
+        <nav class="menu d-flex">
+          <ul class="d-flex">
+            <li v-for="(item, index) in menu" :key="index">
+              <a href="#">{{item.title}}</a>
+            </li>
+          </ul>
+        </nav>
+        <!-- /nav -->
+
+        <!-- logo -->
+        <div class="logo">
+          <img src="../assets/img/boolean-logo.png" alt="logo">
+        </div>
+        <!-- /logo -->
+
+        <!-- icon -->
+        <nav class="menu social">
+          <ul class="d-flex">
+            <li>
+              <a href="#"><i class="fa-regular fa-user"></i></a>
+            </li>
+            <li>
+              <a href="#"><i class="fa-regular fa-heart"></i></a>
+            </li>
+            <li>
+              <a href="#"><i class="fa-solid fa-bag-shopping"></i></a>
+            </li>
+          </ul>
+        </nav>
+        <!-- /icon -->
+
+      </div>
+    </div>
+  </header>
